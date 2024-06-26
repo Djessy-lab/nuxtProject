56ffd3fbd16b3c64fa90cae39f7f0fee689aa314
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-06-26 14:34:46 +0200

    prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@prisma/nuxt", "@nuxt/icon"],
   routeRules: {
-    '/': { prerender: true },
-    '/prerender-multiple/**': { prerender: true },
+    "/": { prerender: true },
+    "/prerender-multiple/**": { prerender: true },
   },
-})
+  alias: {
+    ".prisma/client/index-browser":
+      "./node_modules/@prisma/client/index-browser.js",
+  },
+});
