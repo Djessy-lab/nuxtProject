46575fa596500a9d624de828483db58f81b34849
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-06-28 15:48:11 +0200

    new try

--- server/api/newReport.ts
+++ server/api/newReport.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
+
   const reports = await prisma.report.create({
     data: {
       name: body.name || "John Doe",
