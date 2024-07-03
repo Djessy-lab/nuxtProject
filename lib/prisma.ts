ab3f38910d3e88392122141f58ef2d4bd58947d2
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-07-03 15:07:56 +0200

    wip

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import Prisma, * as PrismaScope from "@prisma/client";
+const PrismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-export type CustomPrismaClient = ReturnType<typeof prismaClientSingleton>;
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+export const PrismaClientKnownRequestError =
+  Prisma?.Prisma.PrismaClientKnownRequestError ||
+  PrismaScope?.Prisma.PrismaClientKnownRequestError;
+export const prismaInstance = new PrismaClient();
+export default prismaInstance;
